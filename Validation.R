9a599cb0e7de5da92aa8cf160237d970bd1514fb
silviapineda <silviapinedasanjuan@gmail.com>
2017-03-23 17:10:32 -0700

    Restricted to maf<0.01 and modifying the RF application Also I have considered the results from Fisher's Exact Test

--- Validation.R
+++ Validation.R
@@ -21,19 +21,24 @@ print(x)
 setwd("/Users/Pinedasans/Catalyst/Results/Validation/")
 
 load("/Users/Pinedasans/Catalyst/Data/Genotyping/Genotyping_QC.Rdata")
-non.list<-seq(1,1316,2)
+non.list<-seq(1,1022,2)
 
 load("/Users/Pinedasans/Catalyst/Data/Genotyping/MAF_HEW.Rdata")
 p.value_hwe_adj<-p.adjust(p.value_hwe,"bonferroni") #389,637 with fdr 131,558 with bonferroni pass the MT correction and are not in HWE
-table(maf<0.05) #12,739 has a maf<0.01 and 138,999 has a maf<0.05
+table(maf<0.01) #12,739 has a maf<0.01 and 138,999 has a maf<0.05
+
+id.maf<-match(names(maf),colnames(SNP_calls_diff2))
+SNP_calls_diff2_maf<-SNP_calls_diff2[,na.omit(id.maf)]
+maf2<-maf[which(is.na(id.maf)==F)]
+SNP_calls_diff_mafQC<-SNP_calls_diff2_maf[,which(maf2>0.01)] #801,977
 
 ##Count number of variants per pair that are DIFFERENT
 variant_mismatch <- NULL
-for (i in 1:658) {
+for (i in 1:511) {
   print(i)
-  variant_mismatch[i]<- table(SNP_calls_diff2[i,]==1)[2]
+  variant_mismatch[i]<- table(SNP_calls_diff_mafQC[i,]==1)[2]
 }
-write.table(variant_mismatch,"/Users/Pinedasans/Catalyst/Results/Validation/variant_mismatch_CANAR.txt")
+write.table(variant_mismatch,"/Users/Pinedasans/Catalyst/Results/Validation/variant_mismatch_MAFfilter.txt")
 
 boxplot(variant_mismatch~annot_samplePaired$Outcome[non.list],frame.plot = FALSE,col=c("goldenrod","darkolivegreen4"),
         ylab="Num Variants Differ")
@@ -75,27 +80,104 @@ SNP_calls_diff_complete<-SNP_calls_diff2[ , ! apply( SNP_calls_diff2, 2 , functi
 save(SNP_calls_diff_complete,annot_samplePaired,file="Validation_RF.Rdata")
 
 
-##Read variants from the discovery set
-resultsRF<-read.table("/Users/Pinedasans/Catalyst/Results/ResultsEndpointRF.txt",header=T)
+###########################################
+#### Apply RF in the Sign<0.05 dataset ###
+#########################################
+library("randomForest")
+library("RColorBrewer")
+resultsFisher<-read.table("/Users/Pinedasans/Catalyst/Results/ResultsEndpointFisherTest.txt",header=T,sep="\t")
 
-OR<-rep(NA,ncol(SNP_calls_diff2))
-p.value.OR<-rep(NA,ncol(SNP_calls_diff2))
-for (i in 1:ncol(SNP_calls_diff2)){
+#Variants that overlap
+id.snp_rs<-match(resultsFisher$snp138,annot$SNP_rs)
+id.snp<-match(annot$SNP_id[na.omit(id.snp_rs)],colnames(SNP_calls_diff_mafQC))
+SNP_rf_selected<-SNP_calls_diff_mafQC[,na.omit(id.snp)]
+
+rf_output_total <- randomForest(annot_samplePaired$Outcome[non.list]~.,data=data.frame(SNP_rf_selected),proximity=TRUE,na.action=na.roughfix)
+
+COLOR=brewer.pal(3,"Set1")
+MDSplot(rf_output_total, annot_samplePaired$Outcome[non.list],palette = COLOR,main = "19variants Overlap")
+legend("bottomleft", legend=levels(annot_samplePaired$Outcome[non.list]),col=COLOR, pch = 20)
+
+###Genes Overlap
+id.gene<-match(resultsFisher$Gene.refGene,annot$gene.name)
+id.snp<-match(annot$SNP_id[na.omit(id.gene)],colnames(SNP_calls_diff_mafQC))
+SNP_rf_selected<-SNP_calls_diff_mafQC[,na.omit(id.snp)] #101 variants
+rf_output_total <- randomForest(annot_samplePaired$Outcome[non.list]~.,data=data.frame(SNP_rf_selected),proximity=TRUE,na.action=na.roughfix)
+
+COLOR=brewer.pal(3,"Set1")
+MDSplot(rf_output_total, annot_samplePaired$Outcome[non.list],palette = COLOR,main = "Genes Overlap")
+legend("bottomleft", legend=levels(annot_samplePaired$Outcome[non.list]),col=COLOR, pch = 20)
+
+###Randomly 101 variants 
+SNP_rf_selected<-SNP_calls_diff_mafQC[,sample(ncol(SNP_calls_diff_mafQC),101)] #101 variants
+rf_output_total <- randomForest(annot_samplePaired$Outcome[non.list]~.,data=data.frame(SNP_rf_selected),proximity=TRUE,na.action=na.roughfix)
+
+COLOR=brewer.pal(3,"Set1")
+MDSplot(rf_output_total, annot_samplePaired$Outcome[non.list],palette = COLOR,main = "101 random variants")
+legend("bottomleft", legend=levels(annot_samplePaired$Outcome[non.list]),col=COLOR, pch = 20)
+
+#RF with  the variants that are sign p<0.001
+###Runnning Logistic Regression to find significant variants
+OR<-rep(NA,ncol(SNP_calls_diff_mafQC))
+p.value.OR<-rep(NA,ncol(SNP_calls_diff_mafQC))
+for (i in 1:ncol(SNP_calls_diff_mafQC)){
   print(i)
   tab<-table(SNP_calls_diff2[,i])
   if(length(tab)>1){
-    model<-glm(annot_samplePaired$Outcome[non.list]~SNP_calls_diff2[,i],family = "binomial")
+    model<-glm(annot_samplePaired$Outcome[non.list]~SNP_calls_diff_mafQC[,i],family = "binomial")
     OR[i]<-exp(coef(model))[2]
     p.value.OR[i]<-coefficients(summary(model))[2,4]
   }
 }
 
-write.table(cbind(OR,p.value),"logisticRegression.txt")
+write.table(cbind(OR,p.value.OR),"logisticRegressionMAFQC.txt")
+results_reg<-read.table("logisticRegressionMAFQC.txt")
+rownames(results_reg)<-colnames(SNP_calls_diff_mafQC)
+results_reg_sign<-results_reg[results_reg$p.value.OR<0.001,] ##1174
+id.snp<-match(rownames(results_reg_sign),colnames(SNP_calls_diff_mafQC))
+SNP_rf_selected<-SNP_calls_diff_mafQC[,na.omit(id.snp)]
 
-results_reg<-read.table("logisticRegression.txt")
-rownames(results_reg)<-colnames(SNP_calls_diff2)
+rf_output_total <- randomForest(annot_samplePaired$Outcome[non.list]~.,data=data.frame(SNP_rf_selected),proximity=TRUE,na.action=na.roughfix)
 
-p.value.adj<-p.adjust(results_reg$p.value,method="fdr") ###None pass MT correction
+COLOR=brewer.pal(3,"Set1")
+MDSplot(rf_output_total, annot_samplePaired$Outcome[non.list],palette = COLOR,main = "SignificantVariants")
+legend("bottomleft", legend=levels(annot_samplePaired$Outcome[non.list]),col=COLOR, pch = 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##Read variants from the discovery set
+resultsFisher<-read.table("/Users/Pinedasans/Catalyst/Results/ResultsEndpointFisherTest.txt",header=T,sep="\t")
+
+results_reg<-read.table("logisticRegressionMAFQC.txt")
+rownames(results_reg)<-colnames(SNP_calls_diff_mafQC)
+id.annot<-match(rownames(results_reg),annot$SNP_id)
+annot_snps<-annot[na.omit(id.annot),]
+
+##SNps that overlap between both datasets
+id.snp<-match(resultsFisher$snp138,annot_snps$SNP_rs) ##19 SNPs overlap 
+results_reg[match(annot_snps[na.omit(id.snp),1],rownames(results_reg)),] ##None Validate
+
+##Genes that overlap
+id.gene<-match(resultsFisher$Gene.refGene,annot_snps$gene.name) #108 variants within the genes that overlap
+results_reg[match(annot_snps[na.omit(id.gene),1],rownames(results_reg)),]
+
+results_reg_sign<-results_reg[match(annot_sign_results$SNP_id,colnames(SNP_calls_diff2)),]
+results_RejvsNorej<-cbind(annot_sign_results,results_reg_sign)
+
+
 SNP_sign<-colnames(SNP_calls_diff2)[which(results_reg$p.value<0.05)]
 id.snp.sign<-match(SNP_sign,annot$SNP_id)
 annot_sign<-annot[id.snp.sign,]
@@ -136,37 +218,12 @@ pheatmap(na.omit(SNP_calls_sign),cluster_rows = T,color = colorRampPalette(c("wh
 
 
 
-###########################################
-#### Apply RF in the Sign<0.05 dataset ###
-#########################################
-library("randomForest")
-library("RColorBrewer")
-resultsRF<-read.table("/Users/Pinedasans/Catalyst/Results/ResultsEndpointRF.txt",header=T)
 
-#Variants that overlap
-id.snp_rs<-match(resultsRF$snp138,annot$SNP_rs)
-id.snp<-match(annot$SNP_id[na.omit(id.snp_rs)],colnames(SNP_calls_diff2))
-SNP_rf_selected<-SNP_calls_diff2[,na.omit(id.snp)]
 
-rf_output_total <- randomForest(annot_samplePaired$Outcome[non.list]~.,data=data.frame(SNP_rf_selected),proximity=TRUE,na.action=na.roughfix)
 
-COLOR=brewer.pal(3,"Set1")
-MDSplot(rf_output_total, annot_samplePaired$Outcome[non.list],palette = COLOR,main = "13variants Overlap")
-legend("bottomleft", legend=levels(annot_samplePaired$Outcome[non.list]),col=COLOR, pch = 20)
 
-###Find the variant in genes that overlap with All RF results
-id.gene<-match(resultsRF$Gene.refGene,annot_sign$gene.name)
-annot_sign_results<-annot_sign[na.omit(id.gene),]
-results_reg_sign<-results_reg[match(annot_sign_results$SNP_id,colnames(SNP_calls_diff2)),]
-results<-cbind(annot_sign_results,results_reg_sign)
-id.snp<-match(results$SNP_id,colnames(SNP_calls_diff2))
-SNP_rf_selected<-SNP_calls_diff2[,na.omit(id.snp)]
 
-rf_output_total <- randomForest(annot_samplePaired$Outcome[non.list]~.,data=data.frame(SNP_rf_selected),proximity=TRUE,na.action=na.roughfix)
 
-COLOR=brewer.pal(3,"Set1")
-MDSplot(rf_output_total, annot_samplePaired$Outcome[non.list],palette = COLOR,main = "Genes Overlap")
-legend("bottomleft", legend=levels(annot_samplePaired$Outcome[non.list]),col=COLOR, pch = 20)
 
 
 ###Find the variant in genes that overlap only with the RejvsNorej
